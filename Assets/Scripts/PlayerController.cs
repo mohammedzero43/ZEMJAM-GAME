34a2f0a1923b4e2944768b920179869a6489b7d0
XtremeZero <mohammedzero43@gmail.com>
2023-06-17 10:08:06 +0400

    Godot update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 public class PlayerController : MonoBehaviour
 {
     public CharacterController cc;
@@ -13,7 +12,6 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed = 5f;
     public float jumpForce = 5f;
 
-    public bool isMoving, isJumping, isRunning;
 
     private float X, Y;
 
@@ -22,26 +20,10 @@ public class PlayerController : MonoBehaviour
         cc = GetComponent<CharacterController>();
         cc.enabled = true;
 
-        //this part is the locking of cursor and to make it visible or no
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        #region Camera Limitation Calculator
-        //Camera limitation variables
-        const float MIN_Y = -60.0f;
-        const float MAX_Y = 70.0f;
-
-        X += Input.GetAxis("Mouse X") * (Sensitivity * Time.deltaTime);
-        Y -= Input.GetAxis("Mouse Y") * (Sensitivity * Time.deltaTime);
-
-        if (Y < MIN_Y)
-            Y = MIN_Y;
-        else if (Y > MAX_Y)
-            Y = MAX_Y;
-        #endregion
-        transform.localRotation = Quaternion.Euler(Y, X, 0.0f);
+ 
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -51,17 +33,15 @@ public class PlayerController : MonoBehaviour
 
         if (cc.isGrounded)
         {
-            isJumping = false;
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                isJumping = true;
                 moveDirection.y = jumpForce;
             }
         }
 
         moveDirection.y += Physics.gravity.y * Time.deltaTime;
-        cc.Move(moveDirection * Time.deltaTime);
+        cc.Move(moveDirection * Time.fixedDeltaTime);
     }
 }
 
